4bb1c39ca84b558b2651a08b16e45f3cb4c709e1
Stavros Mitropoulos <128683063+stavrakas13@users.noreply.github.com>
2025-03-15 13:04:13 +0200

    Update WelcomePage.js

--- front-end/src/pages/WelcomePage.js
+++ front-end/src/pages/WelcomePage.js
@@ -6,9 +6,9 @@ import '../pages/WelcomePage.css';
 function WelcomePage() {
   return (
     <div className="welcome-container">
-      <h1>Καλωσήρθατε στη Διαχείριση Διοδίων!</h1>
-      <p>Χρησιμοποιήστε την εφαρμογή για να δείτε στατιστικά, χάρτες και να διαχειριστείτε πληροφορίες διοδίων.</p>
-      <Link to="/stats" className="welcome-button">Μετάβαση στην Εφαρμογή</Link>
+      <h1>Welcome to the Toll Management System!</h1>
+      <p>Use the app to view statistics, maps, and manage toll information.</p>
+      <Link to="/stats" className="welcome-button">Go to the App</Link>
     </div>
   );
 }
